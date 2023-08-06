0efbd509f2e4b96ee90e6a5bc4c408879f0d4c94
Ayush negi <ayushnegi1912@gmail.com>
2023-08-07 01:28:38 +0530

    movie information working

--- src/component/Movie/Movie.jsx
+++ src/component/Movie/Movie.jsx
@@ -6,8 +6,8 @@ const Movie = ({movie,i}) => {
 	const classes =useStyle();
 	// console.log(movie,i);
   return (
-	<Grid item xs={12} sm={6} md={4} lg={3} xl={2} className={classes.movie}>
-	<Grow in key={i} timeout={(i+1) * 250 } >
+	<Grid item xs={6} sm={6} md={4}  lg={3} xl={2} className={classes.movie}>
+	<Grow in key={i} timeout={(i+1) * 250 }>
 	<Link className={classes.links} to={`/movie/${movie.id}`} >
 		<img className={classes.image}
 			alt={movie.title}
--- src/component/MovieInformation/MovieInformation.jsx
+++ src/component/MovieInformation/MovieInformation.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import {useParams}from 'react-router-dom'
 import useStyles from './styles';
-import {Box, CircularProgress, Grid, Link, Rating, Typography} from '@mui/material';
+import {Box, Button, ButtonGroup, CircularProgress, Grid, Link, Rating, Typography, collapseClasses} from '@mui/material';
+import { Movie as MovieIcon, Theaters, Language, PlusOne, Favorite, FavoriteBorderOutlined, Remove, ArrowBack } from '@mui/icons-material';
+
 import { useGetMovieQuery } from '../../services/TMDB';
+import genreIcons from '../../assets/genres'
 const MovieInformation = () => {
 	const classes =useStyles();
 	const {id} = useParams();
@@ -23,13 +26,14 @@ const MovieInformation = () => {
 		);
 	  }
   return (
-	<Grid  container>
-		<Grid item sm={12} lg={4} align="center">
+	// container use for flex;
+	<Grid  container className={classes.container} >
+		<Grid item sm={12} lg={4} md={4} align="center">
 		<img 
 		src={`https://image.tmdb.org/t/p/w500/${data?.poster_path}`}
 		alt={data?.title}
 		className={classes.poster}
-		 />
+		 />	
 		</Grid>
 		<Grid item container direction ="column" lg={7}>
 		<Typography variant="h3" align="center" gutterBottom >
@@ -38,19 +42,62 @@ const MovieInformation = () => {
 		<Typography variant="h5" align="center" gutterBottom>
           {data?.tagline}
         </Typography>
-
-		<Grid item className={classes.containerSpaceAround}>
+	
+		<Grid className={classes.containerSpaceAround}  item >
 		<Box display="flex" align="center">
 		<Rating readOnly value={data.vote_average/2}/>
 		<Typography variant='subtitle' gutterBottom 
 		style={{marginLeft:'10px'}}>
 		{data?.vote_average}/10
-
 		</Typography>
-
 		</Box>
-
+		<Typography gutterBottom variant="h6" align="center">{data?.runtime}min</Typography>
 		</Grid>
+
+		<Grid item >
+			{data?.genre?.map((genre)=>(
+				<Link className={classes.links} key={genre.name} to="/">
+              <img alt="GENRE NAME" src={genreIcons[genre.name.toLowerCase()]} className={classes.genreImage} height={30} />
+              <Typography color="textPrimary" variant="subtitle1">{genre?.name}</Typography>
+            </Link>
+			))}
+		</Grid>
+		<Typography variant="h5" gutterBottom style={{ marginTop: '20px' }}>Overview</Typography>
+        <Typography style={{ marginBottom: '2rem' }}>{data?.overview}</Typography>
+        <Typography variant="h5" gutterBottom>Top Cast</Typography>
+		 
+		<Grid item container spacing={2}>
+          {data && data?.credits?.cast?.map((character, i) => (
+            character.profile_path && (
+            <Grid key={i} item xs={4} md={2} component={Link} to={`/actors/${character.id}`} style={{ textDecoration: 'none' }}>
+              <img
+                className={classes.castImage}
+                src={`https://image.tmdb.org/t/p/w500/${character.profile_path}`}
+                alt={character.name}
+              />
+              <Typography color="textPrimary" align="center">{character?.name}</Typography>
+              <Typography color="textSecondary" align="center">
+                {character.character.split('/')[0]}
+              </Typography>
+            </Grid>
+            )
+          )).slice(0, 6)}
+        </Grid>
+
+		<Grid item container style={{marginTop:'2rem'}}>
+				<div className={classes.buttonContainer}>
+					<Grid item xs={12} sm={6} className={classes.buttonContainer}>
+					<ButtonGroup size="small" variant="outlined">
+                <Button target="_blank" rel="noopener noreferrer" href={data?.homepage} endIcon={<Language />}>Website</Button>
+                <Button target="_blank" rel="noopener noreferrer" href={`https://www.imdb.com/title/${data?.imdb_id}`} endIcon={<MovieIcon />}>IMDB</Button>
+                {/* <Button onClick={() => setOpen(true)} href="#" endIcon={<Theaters />}>Trailer</Button> */}
+              </ButtonGroup>
+					</Grid>
+				</div>
+		</Grid>		
+
+
+
 
 		</Grid>
 
